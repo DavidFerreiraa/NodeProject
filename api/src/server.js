71b10d1bd7552cbaf3acf130fe52a7c0828258b7
David <Dfl8@aluno.ifal.edu.br>
2024-03-08 11:14:55 -0300

    init: Project api init

---
+++ api/src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello, world")
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
