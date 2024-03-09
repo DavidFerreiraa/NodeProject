ca603ffb121cffd04e31ba96488310c20c6eef87
David <dfl8@aluno.ifal.edu.br>
2024-03-09 02:15:16 -0300

    feat: add post route to /users

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/message/:id", (request, response) => {
     const { id } = request.params;
@@ -11,6 +12,12 @@ app.get("/users", (request, response) => {
     const { page, limit } = request.query;
     response.send(`Página: ${page} - Limite de usuários: ${limit}`);
 });
+
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
+
+    response.json({ name, email, password });
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
