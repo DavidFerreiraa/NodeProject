443b398122d19583be356726012deeda207721bf
David <dfl8@aluno.ifal.edu.br>
2024-03-11 16:19:00 -0300

    feat: add links table migration

--- api/src/database/knex/migrations/20240310072643_createNotes.js
+++ api/src/database/knex/migrations/20240310072643_createNotes.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable("notes", table => {
     table.increments("id");
     table.text("title");
     table.text("description");
+    
     table.integer("user_id").references("id").inTable("users");
 
     table.timestamp("created_at").default(knex.fn.now());
