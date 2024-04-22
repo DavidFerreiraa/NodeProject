469bfd16ebffa55b5b66e214a03ca5c91c0b8d87
David <dfl8@aluno.ifal.edu.br>
2024-04-22 19:01:05 -0300

    feat: add avatar image upload

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
-const ensureAuthenticated = require("../middleware/ensureAuthenticated.js")
+const ensureAuthenticated = require("../middleware/ensureAuthenticated.js");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload.js");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 const usersControllers = new UsersController();
 
 function middleware(request, response, next) {
@@ -12,5 +15,9 @@ function middleware(request, response, next) {
 
 usersRoutes.post("/", usersControllers.create);
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+});
 
 module.exports = usersRoutes;
