5a136139b08d1acab2ebbeb77c5d4a08a2490105
David <dfl8@aluno.ifal.edu.br>
2024-04-22 19:21:33 -0300

    feat: add route to static files in the server

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,8 @@ require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
-const migrationsRun = require("./database/sqlite/migrations")
+const migrationsRun = require("./database/sqlite/migrations");
+const uploadConfig = require("./configs/upload.js");
 
 const app = express();
 
@@ -10,6 +11,9 @@ migrationsRun();
 
 app.use(express.json());
 app.use(routes);
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(( error, request, response, next ) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
