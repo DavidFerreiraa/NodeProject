871f5a720f963c6b1919b6f7f2093bb0fdaed9cf
David <dfl8@aluno.ifal.edu.br>
2024-04-24 00:26:55 -0300

    fix: fix the user_id catch from token

--- api/src/routes/notes.routes.js
+++ api/src/routes/notes.routes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const NotesController = require("../controllers/NotesController.js");
-const ensureAuthnticated = require("../middleware/ensureAuthenticated.js");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated.js");
 const notesRoutes = Router();
 const notesController = new NotesController();
 
-notesRoutes.use(ensureAuthnticated);
+notesRoutes.use(ensureAuthenticated);
 
 notesRoutes.get("/", notesController.index);
-notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.post("/", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
 
