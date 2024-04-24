dc491ec7eec4f711194a919da992ece874802c0e
David <dfl8@aluno.ifal.edu.br>
2024-04-24 01:26:25 -0300

    fix: fix the duplication of notes in NotesController index

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -20,6 +20,7 @@ class NotesController {
             .whereLike("notes.title", `%${search}%`)
             .whereIn("name", filterTags)
             .innerJoin("notes", "notes.id", "tags.note_id")
+            .groupBy("notes.title")
             .orderBy("notes.title");
         } else {
             notes = await knex("notes")
